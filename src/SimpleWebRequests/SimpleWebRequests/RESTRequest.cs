4593e463c3b8c8fc746566f4b80b61f49298542f
Denis Babarykin <babarykin.denis@gmail.com>
2016-10-09 03:53:23 +0300

    Generic methods added

--- src/SimpleWebRequests/SimpleWebRequests/RESTRequest.cs
+++ src/SimpleWebRequests/SimpleWebRequests/RESTRequest.cs
@@ -210,5 +210,140 @@ namespace SimpleWebRequests
         }
 
         #endregion
+
+
+        #region Generic methods
+
+        /// <summary>GET request to the api that responses in JSON format</summary>
+        /// <param name="url">String with url and parameters for GET request</param>
+        /// <returns>Object of given generic type with response data</returns>
+        public static T GetWithJsonResponse<T>(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    response.Content.Headers.ContentType.MediaType = "application/json";
+                    return response.Content.ReadAsAsync<T>().Result;
+                }
+                else
+                    throw new HttpRequestException(string.Format("Error in {0} with param url: {1}", url, MethodBase.GetCurrentMethod().Name));
+            }
+        }
+
+        /// <summary>Async GET request to the api that responses in JSON format</summary>
+        /// <param name="url">String with url and parameters for GET request</param>
+        ///<returns>Object of given generic type with response data</returns>
+        public static async Task<T> GetWithJsonResponseAsync<T>(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    response.Content.Headers.ContentType.MediaType = "application/json";
+                    return await response.Content.ReadAsAsync<T>();
+                }
+                else
+                    throw new HttpRequestException(string.Format("Error in {0} with param url: {1}", url, MethodBase.GetCurrentMethod().Name));
+            }
+        }
+
+        /// <summary>POST request with body of "x-www-form-urlencoded" type to the api that responses in JSON</summary>
+        /// <param name="url">String with url for POST request</param>
+        /// <param name="postRequestBody">Body of POST request that will be serialized to "application/x-www-form-urlencoded" media type</param>
+        /// <returns>Object of given generic type with response data</returns>
+        public static T PostAsUrlEncodedWithJsonResponse<T>(string url, IEnumerable<KeyValuePair<string, string>> postRequestBody)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = client.PostAsync(url, new FormUrlEncodedContent(postRequestBody)).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    response.Content.Headers.ContentType.MediaType = "application/json";
+                    return response.Content.ReadAsAsync<T>().Result;
+                }
+                else
+                    throw new HttpRequestException(string.Format("Error in {0} with params url: {1} and body: {2} ", MethodBase.GetCurrentMethod().Name, url, postRequestBody.ToString()));
+            }
+        }
+
+        /// <summary>Async POST request with body of "x-www-form-urlencoded" type to the api that responses in JSON</summary>
+        /// <param name="url">String with url for POST request</param>
+        /// <param name="postRequestBody">Body of POST request that will be serialized to "application/x-www-form-urlencoded" media type</param>
+        /// <returns>Object of given generic type with response data</returns>
+        public static async Task<T> PostAsUrlEncodedWithJsonResponseAsync<T>(string url, IEnumerable<KeyValuePair<string, string>> postRequestBody)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.PostAsync(url, new FormUrlEncodedContent(postRequestBody));
+                if (response.IsSuccessStatusCode)
+                {
+                    response.Content.Headers.ContentType.MediaType = "application/json";
+                    return await response.Content.ReadAsAsync<T>();
+                }
+                else
+                    throw new HttpRequestException(string.Format("Error in {0} with params url: {1} and body: {2} ", MethodBase.GetCurrentMethod().Name, url, postRequestBody.ToString()));
+            }
+        }
+
+        /// <summary>POST request with body of JSON type to the api that responses in JSON</summary>
+        /// <param name="url">String with url for POST request</param>
+        /// <param name="postRequestBody">Body of POST request that will be serialized to JSON</param>
+        /// <returns>Object of given generic type with response data</returns>
+        public static T PostAsJsonWithJsonResponse<T>(string url, object postRequestBody)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = client.PostAsync(url, new StringContent(JsonConvert.SerializeObject(postRequestBody), Encoding.UTF8, "application/json")).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    response.Content.Headers.ContentType.MediaType = "application/json";
+                    return response.Content.ReadAsAsync<T>().Result;
+                }
+                else
+                    throw new HttpRequestException(string.Format("Error in {0} with params url: {1} and body: {2} ", MethodBase.GetCurrentMethod().Name, url, postRequestBody.ToString()));
+            }
+        }
+
+        /// <summary>Async POST request with body of JSON type to the api that responses in JSON</summary>
+        /// <param name="url">String with url for POST request</param>
+        /// <param name="postRequestBody">Body of POST request that will be serialized to JSON</param>
+        /// <returns>Object of given generic type with response data</returns>
+        public static async Task<T> PostAsJsonWithJsonResponseAsync<T>(string url, object postRequestBody)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.PostAsync(url, new StringContent(await Task.Factory.StartNew(() => JsonConvert.SerializeObject(postRequestBody)), Encoding.UTF8, "application/json"));
+                if (response.IsSuccessStatusCode)
+                {
+                    response.Content.Headers.ContentType.MediaType = "application/json";
+                    return await response.Content.ReadAsAsync<T>();
+                }
+                else
+                    throw new HttpRequestException(string.Format("Error in {0} with params url: {1} and body: {2} ", MethodBase.GetCurrentMethod().Name, url, postRequestBody.ToString()));
+            }
+        }
+
+        #endregion
     }
 }
