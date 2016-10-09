7caf3a183479855b45070f969e6d6673a326202a
Denis Babarykin <babarykin.denis@gmail.com>
2016-10-09 04:34:50 +0300

    Nuget release fixed

--- src/SimpleWebRequests/SimpleWebRequests/Properties/AssemblyInfo.cs
+++ src/SimpleWebRequests/SimpleWebRequests/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleWebRequests")]
-[assembly: AssemblyDescription("Package for simple web requests. Allows to asynchronously send GET and POST requests with one line of code and get response in dynamic object.")]
+[assembly: AssemblyDescription("Package for simple web requests. Allows to synchronously or asynchronously send GET and POST requests with one line of code and get response in specified or dynamic object.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Denis Babarykin")]
 [assembly: AssemblyProduct("SimpleWebRequests")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
